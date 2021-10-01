c36fc4370ddd90cae1890cc3e4cdb3660ca0c05f
francisco <franciscothestanley@gmail.com>
2021-10-01 20:12:15 -0300

    criando models do db

---
+++ core/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class PontoTuristico(models.Model):
+    nome = models.CharField(max_length=150)
+    descricao = models.TextField()
+    aprovado = models.BooleanField()
+
+    def __str__(self):
+        return self.nome
