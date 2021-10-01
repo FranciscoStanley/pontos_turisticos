cf063ea1acac3044ba95a675e6473cc12ab4c048
francisco <franciscothestanley@gmail.com>
2021-10-01 20:32:01 -0300

    criando models do db. Criando files da api.

--- core/models.py
+++ core/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class PontoTuristico(models.Model):
     nome = models.CharField(max_length=150)
     descricao = models.TextField()
-    aprovado = models.BooleanField()
+    aprovado = models.BooleanField(default=False)
 
     def __str__(self):
         return self.nome
