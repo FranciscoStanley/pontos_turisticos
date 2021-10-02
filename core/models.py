48d8e54fc9427e1fb1739965d443da05ca7f6b5e
francisco <franciscothestanley@gmail.com>
2021-10-02 08:11:34 -0300

    criando models de avalições

--- core/models.py
+++ core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from atracoes.models import Atracao
 from comentarios.models import Comentario
+from avaliacoes.models import Avalicao
 
 class PontoTuristico(models.Model):
     nome = models.CharField(max_length=150)
@@ -8,6 +9,7 @@ class PontoTuristico(models.Model):
     aprovado = models.BooleanField(default=False)
     atracoes = models.ManyToManyField(Atracao)
     comentarios = models.ManyToManyField(Comentario)
+    avalicoes = models.ManyToManyField(Avalicao)
 
     def __str__(self):
         return self.nome
