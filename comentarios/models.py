633b2579ad203c8b7c3fe88baa4166a7f6f44cd7
francisco <franciscothestanley@gmail.com>
2021-10-02 07:44:38 -0300

    criando a api da app comentarios

--- comentarios/models.py
+++ comentarios/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Comentario(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    comentario = models.TextField()
+    data = models.DateTimeField(auto_now=True)
+    aprovado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.usuario.first_name
