18410b0fa4ddf54b0d4baf25f85c275c772a98db
francisco <franciscothestanley@gmail.com>
2021-10-10 19:00:35 -0300

    refatorando

--- avaliacoes/models.py
+++ avaliacoes/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-class Avalicao(models.Model):
+class Avaliacao(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     comentario = models.TextField(null=True, blank=True)
     nota = models.DecimalField(max_digits=3, decimal_places=2)
