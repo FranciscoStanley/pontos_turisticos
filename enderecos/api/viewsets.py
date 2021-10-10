8538a972688c5df3df5dd879dc1c87072f0dc296
francisco <franciscothestanley@gmail.com>
2021-10-10 18:00:58 -0300

    criando EndPoint de enderecos, viewsets e serialzers.

---
+++ enderecos/api/viewsets.py
@@ -0,0 +1,7 @@
+from rest_framework.viewsets import ModelViewSet
+from enderecos.models import Endereco
+from .serializers import EnderecoSerializer
+
+class EnderecoViewSet(ModelViewSet):
+    queryset = Endereco.objects.all()
+    serializer_class = EnderecoSerializer
--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -19,10 +19,12 @@ from django.conf.urls import url, include
 from rest_framework import routers
 from core.api.viewsets import PontoTuristicoViewSet
 from atracoes.api.viewsets import AtracoesViewSet
+from enderecos.api.viewsets import EnderecoViewSet
 
 router = routers.DefaultRouter()
 router.register(r'pontoturistico', PontoTuristicoViewSet)
 router.register(r'atracoes', AtracoesViewSet)
+router.register(r'enderecos', EnderecoViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
