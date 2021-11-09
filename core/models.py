6b356cd82c552e9f6a392b89f17ca720bac1c5c0
francisco <franciscothestanley@gmail.com>
2021-11-08 21:19:58 -0300

    add imagens

--- core/models.py
+++ core/models.py
@@ -12,6 +12,7 @@ class PontoTuristico(models.Model):
     comentarios = models.ManyToManyField(Comentario)
     avaliacoes = models.ManyToManyField(Avaliacao)
     enderecos = models.ForeignKey(Endereco, on_delete=models.CASCADE, null=True, blank=True)
+    fotos = models.ImageField(upload_to='pontos_turisticos', null=True, blank=True)
 
     def __str__(self):
         return self.nome
