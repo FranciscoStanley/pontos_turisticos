5034427d95d7221df7231313139b657bed2cc860
francisco <franciscothestanley@gmail.com>
2021-11-11 18:57:14 -0300

    Habilitando o mecanismo de tokens para autenticação

--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'django_filters',
+    'rest_framework.authtoken',
     'core',
     'atracoes',
     'comentarios',
@@ -131,7 +132,11 @@ MEDIA_ROOT = 'imagens'
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
-    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly']
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
 }
 
 # Default primary key field type
